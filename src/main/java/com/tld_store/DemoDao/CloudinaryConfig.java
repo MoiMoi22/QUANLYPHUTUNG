4e1aab31f06148dd5523dca9d38e6f6a45d359c0
MoiMoi22 <linhpham2003zx@gmail.com>
2025-07-08 20:27:09 +0700

    Avoid exposing cloudinary api key

--- src/main/java/com/tld_store/DemoDao/CloudinaryConfig.java
+++ src/main/java/com/tld_store/DemoDao/CloudinaryConfig.java
@@ -2,18 +2,28 @@ package com.tld_store.DemoDao;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CloudinaryConfig {
+    @Value("${cloudinary.cloud_name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api_key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api_secret}")
+    private String apiSecret;
 
     @Bean
     public Cloudinary cloudinary() {
         return new Cloudinary(ObjectUtils.asMap(
-            "cloud_name", "dvj8jmk6e",
-            "api_key", "684837669696959",
-            "api_secret", "x_uDrcstr2TFVO47YgkygjwhR7Q"
+            "cloud_name", cloudName,
+            "api_key", apiKey,
+            "api_secret", apiSecret
         ));
     }
 }
